935d4095429980d833e3c33849dfc6506d79c2db
Kevin Baugh <kbaugh@indeed.com>
2019-01-21 15:49:31 -0600

    Added a 200 file and ignored the company theme.

--- _assets/scripts/.transpiled.js
+++ _assets/scripts/.transpiled.js
@@ -3,8 +3,24 @@
     factory();
 }(function () { 'use strict';
 
+    // This should be a dynamic import. "If a legacy browser, import the polyfills."
+    // Might be wonky in the transpiler....
     // import './polyfills.js'
 
     window.iterations = (maxIterations) => { return Array(maxIterations).fill(0).map((n,i) => i) };
+
+    (async () => {
+        console.log("Start here.                                        --------------->");
+        // const url = document.querySelector('input').value
+        // const jsonResponse = await fetch(url).then(r => r.json())
+        // document.querySelector('fetch-result').innerHTML = `<pre><code>${JSON.stringify(jsonResponse, null, 2)}</code></pre>`
+        // for(let idx of iterations(10)) {
+        //     console.log(idx)
+        // }
+        // const [ a, b ] = iterations(2)
+        // console.log(a, b)
+
+        // document.querySelector('.dialog-button').addEventListener('click', (e) => { e.preventDefault(); document.querySelector('dialog').toggleAttribute('open') })
+    })();
 
 }));
--- _assets/scripts/main.js
+++ _assets/scripts/main.js
@@ -1,8 +1,11 @@
+// This should be a dynamic import. "If a legacy browser, import the polyfills."
+// Might be wonky in the transpiler....
 // import './polyfills.js'
 
 window.iterations = (maxIterations) => { return Array(maxIterations).fill(0).map((n,i) => i) }
 
 (async () => {
+    console.log("Start here.                                        --------------->")
     // const url = document.querySelector('input').value
     // const jsonResponse = await fetch(url).then(r => r.json())
     // document.querySelector('fetch-result').innerHTML = `<pre><code>${JSON.stringify(jsonResponse, null, 2)}</code></pre>`
